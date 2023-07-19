50180ec39400f2627e1e589376eaf542bbd4988e
gjkoni <gj_koni@hotmail.com>
2023-07-19 18:59:28 +0800

    add lottie

---
+++ components/lottie/lottie.vue
@@ -0,0 +1,33 @@
+<template>
+	<div ref="bodyAnimation"></div>
+</template>
+
+<script>
+	import lottie from "lottie-web";
+	export default {
+		name: "lottie",
+		data() {
+			return {
+			};
+		},
+		mounted() {
+			this.anim = lottie.loadAnimation({
+				container: this.$refs.bodyAnimation, // the dom element that will contain the animation
+				renderer: "svg",
+				loop: false,
+				autoplay: false,
+				prerender: true,
+				path: `../../static/lottie/p${this.getRandom(1, 20)}.json`
+			});
+			this.$emit("animCreated", this.anim);
+		},
+		methods: {
+			getRandom(min, max) {
+				return Math.floor(Math.random() * (max - min + 1)) + min;
+			}
+		},
+	};
+</script>
+
+<style lang="scss" scoped>
+</style>
--- pages/index/index.vue
+++ pages/index/index.vue
@@ -2,24 +2,28 @@
 	<view class="container" :style="`--size:${size}px;--zindex:-1;`">
 		<!-- <router-link to="/pages/settings/settings">{{ $t('settings.name') }}</router-link> -->
 		<view class="tian"></view>
-		<view id="completesvg" v-if="success"></view>
+		<lottie v-if="success" class="lottier" @animCreated="lottieAnimation" />
 	</view>
 </template>
 
 <script setup>
 	import cnchar from 'cnchar-all';
-	
-	import { onLoad } from '@dcloudio/uni-app'
+
+	import {
+		onLoad
+	} from '@dcloudio/uni-app'
 
 	import {
 		ref,
 		reactive,
-		onMounted
+		onMounted,
+		nextTick
 	} from 'vue'
-	
+
 	const text = ref('')
 	const size = ref(300)
 	const success = ref(false)
+	const lottier = ref(null)
 	const drawScreen = reactive({
 		screenWidth: window.innerWidth
 	})
@@ -38,56 +42,34 @@
 		},
 		test: {
 			drawingWidth: 50,
-			onTestStatus: function(args) {
-				if (args.status === "complete") {
-					success.value = true
-					// var animData = {
-					//   container: document.getElementById("completesvg"),
-					//   renderer: "svg",
-					//   loop: false,
-					//   prerender: true,
-					//   autoplay: true,
-					//   path: `static/lottie/p${getRandom(1, 20)}.json`,
-					// };
-					// anim = bodymovin.loadAnimation(animData);
-					// anim.onComplete = () => {
-					//   anim.destroy();
-					//   document.getElementById("completesvg").style.display = "none";
-					//   totalStrokes = 0;
-					// };
-				} else if (args.status === "mistake") {
-					// console.log("error");
-					// console.log("已画" + args.data.strokeNum + "笔");
-					// console.log("剩余" + args.data.strokesRemaining + "笔");
-					// console.log(
-					//   "当前应画笔画错误第" +
-					//   args.data.mistakesOnStroke +
-					//   "次（当上一笔写对后，本次从0开始记）"
-					// );
-					// console.log("总共写错次数" + args.data.totalMistakes + "次");
-					// totalStrokes = args.data.strokeNum + args.data.strokesRemaining;
-				}
-			},
+			onTestStatus: testStatus
 		},
 	})
-	
-	onLoad((querys) =>{
-		console.log(drawScreen.screenWidth)
-		if(querys.text){
+
+	onLoad((querys) => {
+		if (querys.text) {
 			text.value = querys.text
-		}
-		if(querys.size){
-			size.value = querys.size
 		}else{
-			size.value = drawScreen.screenWidth / (text.value.length < 5 ? text.value.length : 5)
-			console.log(size.value)
+			var queryText = GetQueryString('text')
+			text.value = queryText  !== '' ? queryText : "中国"
+		}
+		if (querys.size) {
+			size.value = querys.size
+		} else {
+			var querySize = GetQueryString('size')
+			size.value = querySize !== '' ? Number(querySize) : drawScreen.screenWidth / (text.value.length < 5 ? text.value.length : 5)
 			//drawOptions.style.length = Math.floor(size.value)
 			drawOptions.style.length = Math.floor(size.value) - 2 //减去svg的style边框宽度，否则会超出宽度导致换行
 		}
 	})
 
 	onMounted(() => {
-		cnchar.setResourceBase('http://localhost:12321/')
+		const app = getApp().globalData.env
+		if(app.env === 'production'){
+			cnchar.setResourceBase('cnchar-data/')
+		}else{
+			cnchar.setResourceBase('http://localhost:8080/')
+		}
 
 		cnchar.draw(text.value, drawOptions);
 
@@ -95,6 +77,47 @@
 			console.log(word);
 		});
 	})
+	
+	function testStatus(args){
+		if (args.status === "complete") {
+			success.value = true
+			nextTick(()=>{
+			      lottier.value.play();
+			    })
+			
+		} else if (args.status === "mistake") {
+			// console.log("error");
+			// console.log("已画" + args.data.strokeNum + "笔");
+			// console.log("剩余" + args.data.strokesRemaining + "笔");
+			// console.log(
+			//   "当前应画笔画错误第" +
+			//   args.data.mistakesOnStroke +
+			//   "次（当上一笔写对后，本次从0开始记）"
+			// );
+			// console.log("总共写错次数" + args.data.totalMistakes + "次");
+			// totalStrokes = args.data.strokeNum + args.data.strokesRemaining;
+		}
+	}
+	
+	function GetQueryString(name) {
+	      var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)", "i");
+	      var r = window.location.search.substr(1).match(reg); //获取url中"?"符后的字符串并正则匹配
+	      var context = "";
+	      if (r != null) context = decodeURIComponent(r[2]);
+	      reg = null;
+	      r = null;
+	      return context == null || context == "" || context == "undefined"
+	        ? ""
+	        : context;
+	    }
+	
+	function lottieAnimation (anim) {
+      lottier.value = anim
+	  lottier.value.onComplete = () => {
+	    lottier.value.destroy()
+		success.value = false
+	  };
+    }
 </script>
 
 <style lang="scss" scoped>
@@ -117,27 +140,22 @@
 			align-items: flex-start;
 			flex-wrap: wrap;
 			background-color: transparent;
-			
+
 			svg {
 				box-sizing: border-box;
 				width: var(--size);
 				height: var(--size);
 				background-color: transparent !important;
 			}
-			
-			// img {
-			// 	width: 100%;
-			// 	height: 100%;
-			// }
 		}
-
-		#completesvg {
-			position: absolute;
-			width: 100%;
-			height: 100%;
-			left: 0;
-			right: 0;
+		
+		.lottier{
+			position: fixed;
 			bottom: 0;
+			margin: 0 auto;
+			overflow: hidden;
+			width: 100vw;
+			height: 100vw;
 			z-index: var(--zindex);
 		}
 	}
